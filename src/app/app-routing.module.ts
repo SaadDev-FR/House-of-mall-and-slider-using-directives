c0abf53f399f4feb588fd4d4406b3ae57750e2ca
Saad Nazir <rajasaadnazir20@gmail.com>
2022-12-08 03:10:21 +0500

    landing page cover ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'app-landing-page', pathMatch: 'full' },
+  { path: 'app-landing-page', component: LandingPageComponent },
+  { path: 'app-navbar', component: NavbarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
